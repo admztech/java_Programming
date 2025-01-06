4ce14bd637e5ede2d2d9c9d915c9526b426115e3
Adnan <adnandesign47@gmail.com>
2025-01-06 20:57:32 +0500

    do some pratice

--- JavaBasic.java
+++ JavaBasic.java
@@ -1,5 +1,37 @@
+import java.util.*;
+
 public class JavaBasic {
     public static void main(String args[]) {
-        System.out.print("Hellow World");
+
+        Scanner scr = new Scanner(System.in);
+
+        float a = 25.12f;
+        int b = (int) a;
+        System.out.println(b);
+
+        /*
+         * The are of the circle 2Pi(r*r)
+         * float rad = scr.nextFloat();
+         * float area = 2* 3.14f * rad * rad ;
+         * System.out.println(area);
+         */
+        /*
+         * Product of two number
+         * 
+         * int a = scr.nextInt();
+         * int b = scr.nextInt();
+         * int product = a * b;
+         * System.out.println(product);
+         */
+
+        /*
+         * summ of the two number
+         * int a = scr.nextInt();
+         * int b = scr.nextInt();
+         * int sum = a+b;
+         * 
+         * System.outprintln(sum);
+         */
+
     }
 }
