aa235dfa23dddeefc3c67fa493302f6e88147d5f
admztech <adnandesign47@gmail.com>
2025-01-19 19:17:06 +0500

    function in java

---
+++ Functions/useFunction.java
@@ -0,0 +1,28 @@
+package Functions;
+
+import java.util.*;
+
+public class useFunction {
+    // Produt of Two number;
+    public static int produtTWoNumber(int num1, int num2) {
+        int product = (num1 * num2);
+        return product;
+    }
+
+    // Calculate Factorial of a number
+    // public static int factorial(int n) {
+    // int f = 1;
+    // for (int i = 1; i <= n; i++) {
+    // f *= i;
+    // }
+    // return f;
+    // }
+
+    public static void main(String args[]) {
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Enter The First Number: ");
+        int num = scr.nextInt();
+       
+
+    }
+}
