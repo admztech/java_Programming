c0bbc7a49d64e8faadadce74c82e544375d2e994
admztech <adnandesign47@gmail.com>
2025-01-22 16:21:52 +0500

    Butterfly pattern completed

--- Pattern/Butterfly.java
+++ Pattern/Butterfly.java
@@ -6,15 +6,31 @@ public class Butterfly {
     public static void butterlfyPrint(int n) {
         // first half outter loop
         for (int i = 1; i <= n; i++) {
-            // print the stars
+            // print the stars -i
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
-            // print the spaces
+            // print the spaces - 2*(n-i)
             for (int s = 1; s <= 2 * (n - i); s++) {
                 System.out.print(" ");
             }
-            // print the start
+            // print the start -i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // 2nd half part
+        for (int i = n; i >= 1; i--) {
+            // start print -i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // space print-2(n-i)
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // start -i
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
