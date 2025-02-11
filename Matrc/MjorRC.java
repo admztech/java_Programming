2fe0225f89a6e9f2e5c69783d93aa3dfec8f4b30
admztech <adnandesign47@gmail.com>
2025-02-11 12:03:25 +0500

    find sum of Major row in Matrix

--- Matrc/MjorRC.java
+++ Matrc/MjorRC.java
@@ -23,7 +23,7 @@ public class MjorRC {
 
     public static void main(String[] args) {
         int matrix[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-        printMajorRC(matrix);
+        printMajorRS(matrix);
 
     }
 }
