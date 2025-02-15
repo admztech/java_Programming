e9ea6c20aa8a5dde83846e3d4860c951d49f4896
admztech <adnandesign47@gmail.com>
2025-02-15 23:21:37 +0500

    Intern method check

---
+++ String/InernMethod.java
@@ -0,0 +1,15 @@
+package String;
+
+import java.util.*;
+
+public class InernMethod {
+
+    public static void main(String[] args) {
+        String a = "helloe";
+        String b = a;
+        a = "Hello===";
+        System.out.println("this is a value " + a);
+        System.out.println("this is b value " + b);
+
+    }
+}
